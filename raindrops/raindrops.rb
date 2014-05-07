1284081d41fecd84096092eb8cd8e5a220c51af0
Ballauriena <lleecrowe@gmail.com>
2014-05-07 16:11:47 -0500

    changed += to shovels at the request of a nit-picker

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -10,10 +10,10 @@ class Raindrops
 
 	def self.convert(num)
 		string = ""
-		string += "Pling" if num.divisible?(3)
-		string += "Plang" if num.divisible?(5)
-		string += "Plong" if num.divisible?(7)
-		string += num.to_s if string.empty?
+		string << "Pling" if num.divisible?(3)
+		string << "Plang" if num.divisible?(5)
+		string << "Plong" if num.divisible?(7)
+		string << num.to_s if string.empty?
 		string
 	end
 
